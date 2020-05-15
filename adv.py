1602fb529b7a38d5a6dacc71a96abbfeb8561e3d
Samantha Egge <samantha.c.egge@gmail.com>
2020-05-15 07:37:58 -0700

    Working on traversal logic Currently not moving outside of initial square.

--- adv.py
+++ adv.py
@@ -2,6 +2,7 @@ from room import Room
 from player import Player
 from world import World
 from util import Queue, Stack
+from mapper import Mapper
 
 import random
 from ast import literal_eval
@@ -25,12 +26,7 @@ world.load_graph(room_graph)
 # Print an ASCII map
 world.print_rooms()
 
-player = Player(world.starting_room)
-
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
-
+mapper = Mapper(world.starting_room)
 
 ## Map Dictionary
 # this is a map of every room in the graph and the directions it has in order to use it to help with traversal while walking the maze
@@ -48,8 +44,6 @@ opposites = {
   's': 'n',
   'w': 'e'
 }
-
-
 
 ## Cannot use stack??
 # stack:{000, E}, {104, S}
@@ -73,20 +67,20 @@ def discover_map():
   
   while len(map_dict) < 499 or len(discover_string) > 0:
     ## Gets current set of exits
-    exits = player.current_room.get_exits()
+    exits = mapper.current_room.get_exits()
     ## check the current room agains the dictionary
-    if player.current_room.id not in map_dict:
+    if mapper.current_room.id not in map_dict:
       new_data = {}
       # Develops new entry in map dict for new nodes discovered
-      map_dict[player.current_room.id] = {}
+      map_dict[mapper.current_room.id] = {}
       # This only triggers on first room
       if prev_room is None:
         for exit in exits:
-          # new_tuple = (player.current_room.id, exit)
+          # new_tuple = (mapper.current_room.id, exit)
           new_data[exit] = '?'
         ## Just need to know which room to attend. Check at end if there's still question marks before moving.
-        # queue.add(player.current_room.id)
-        map_dict[player.current_room.id] = new_data
+        # queue.add(mapper.current_room.id)
+        map_dict[mapper.current_room.id] = new_data
 
       # This should trigger for each new node discovered
       else:
@@ -94,24 +88,24 @@ def discover_map():
           ## This sets the dictionary to point at the past room
           # Then it also connects current room
           if opposites[direction_traveled] == exit:
-            map_dict[prev_room][direction_traveled] = player.current_room.id
-            map_dict[player.current_room.id][opposites[direction_traveled]] = prev_room
+            map_dict[prev_room][direction_traveled] = mapper.current_room.id
+            map_dict[mapper.current_room.id][opposites[direction_traveled]] = prev_room
           else:
             # Build exits, flip boolean so I know I have new nodes to explore
             untouched_nodes = True
-            map_dict[player.current_room.id][exit] = '?'
+            map_dict[mapper.current_room.id][exit] = '?'
     #This Node exists, so we need to examine the current dictionary and see if we need to update anything
     else:
       for exit in exits:
         ## Adds connecting paths
         # if this was previous direction traveled, updated prev node and current node to path (might already be done)
         if opposites[direction_traveled] == exit:
-          map_dict[prev_room][direction_traveled] = player.current_room.id
-          map_dict[player.current_room.id][opposites[direction_traveled]] = prev_room
+          map_dict[prev_room][direction_traveled] = mapper.current_room.id
+          map_dict[mapper.current_room.id][opposites[direction_traveled]] = prev_room
         ## Creates new zones to the queue, and adds untouched paths to dictionary
-        elif exit not in map_dict[player.current_room.id]:
+        elif exit not in map_dict[mapper.current_room.id]:
           untouched_nodes = True
-          map_dict[player.current_room.id][exit] = '?'
+          map_dict[mapper.current_room.id][exit] = '?'
         
     ## Now that we've dealt with the dictionary and queue we need to move to the next room
     possible_directions = []
@@ -120,20 +114,20 @@ def discover_map():
     # This is finding all possible exits, then adding those to our possibilities
     # print('map_dict', map_dict)
     for exit in exits:
-      if map_dict[player.current_room.id][exit] is '?':
+      if map_dict[mapper.current_room.id][exit] is '?':
         possible_directions.append(exit)
     
     ## If there's possible directions, we're going to roll those directions and then move into it
     if len(possible_directions) > 0:
-      queue.discard(player.current_room.id)
+      queue.discard(mapper.current_room.id)
       if len(possible_directions) > 1:
-        queue.add(player.current_room.id)
+        queue.add(mapper.current_room.id)
       # print('we have possible directions', possible_directions)
       direction_roll = possible_directions[random.randint(0, len(possible_directions)-1)]
       discover_string.append(direction_roll)
-      prev_room = player.current_room.id
+      prev_room = mapper.current_room.id
       direction_traveled = direction_roll
-      player.travel(direction_roll)
+      mapper.travel(direction_roll)
 
     ## If we don't have possible directions we need to move backwards through our string trail until we do
     else:
@@ -142,41 +136,242 @@ def discover_map():
         # print(queue)
         if len(discover_string) == 0:
           return
-        queue.discard(player.current_room.id)
+        queue.discard(mapper.current_room.id)
         last_discover = discover_string[len(discover_string)-1]
-        player.travel(opposites[last_discover])
+        mapper.travel(opposites[last_discover])
         discover_string.pop(len(discover_string)-1)
-        move_exits = player.current_room.get_exits()
+        move_exits = mapper.current_room.get_exits()
         for exit in move_exits:
-          if map_dict[player.current_room.id][exit] is '?':
+          if map_dict[mapper.current_room.id][exit] is '?':
             possible_directions.append(exit)
         # print('random move')
         # random_dir = move_exits[random.randint(0, len(move_exits)-1)]
-        # player.travel(random_dir)
+        # mapper.travel(random_dir)
       direction_roll = possible_directions[random.randint(0, len(possible_directions)-1)]
       discover_string.append(direction_roll)
-      prev_room = player.current_room.id
+      prev_room = mapper.current_room.id
       direction_traveled = direction_roll
-      player.travel(direction_roll)
+      mapper.travel(direction_roll)
   print('we found all 500 rooms')
 
+# LET'S DO THE REAL THING
+dead_ends = {}
+
+def find_deadends(full_map):
+  ## This method uses the map_dict to determine traversal path
+  ## It finds all dead ends
+  ## It back tracks for each dead end to it's pivot location (next room with 3 exits)
+  prev_direction = None
+
+  opposites = {
+    'n': 's',
+    'e': 'w',
+    's': 'n',
+    'w': 'e'
+  }
+
+  direction_room = None
+  prev_direction = None
+  prev_room = None
+
+  # This grabs every dead end room id
+  ## This should be recursion if I had 1 sliver of a brain
+  for room in map_dict:
+    num_exits = map_dict[room]
+    if len(num_exits) == 1:
+      prev_room = room
+      for key in map_dict[room]:
+        prev_direction = key
+      direction_room = map_dict[room][prev_direction]
+      while len(map_dict[direction_room]) is 2:
+        for path in map_dict[direction_room]:
+          ## Makes sure you aren't moving backwards. Opposite choice, should move you back down the path you came from
+          if path not in map_dict[direction_room]:
+            print('fuck you')
+          elif map_dict[direction_room][path] != prev_room:
+            prev_room = direction_room
+            prev_direction = path
+            direction_room = map_dict[prev_room][prev_direction]
+
+      if direction_room in dead_ends:
+        dead_ends[direction_room].append(opposites[prev_direction])
+      else:
+        dead_ends[direction_room] = [opposites[prev_direction]]
+  print('dead-ends', dead_ends)
+
+
+##################################### STARTING TRAVERSAL
+## Labyrinth String
+# Add each step made through the maze, until no new directions available (and not pivot paths). Start at end of lab string and step back until stack direction is available
+# lab_string = []
+
+# # Fill this out with directions to walk
+# # traversal_path = ['n', 'n']
+# traversal_path = []
+
+# player = Player(world.starting_room)
+
+# # TRAVERSAL TEST - DO NOT MODIFY
+# visited_rooms = set()
+# player.current_room = world.starting_room
+# visited_rooms.add(player.current_room)
+
+def warrior_traversal():
+  ### INITIALIZE ALL WARRIOR VARIABLES
+  ## RESET PLAYER
+  ## NEW TRAVERSAL PATH
+  ## RESET VISITED_ROOMS
+  lab_string = []
+
+  ## Emergency pullchute
+  queue = []
+
+  # Fill this out with directions to walk
+  # traversal_path = ['n', 'n']
+  traversal_path = []
+
+  player = Player(world.starting_room)
+
+  # TRAVERSAL TEST - DO NOT MODIFY
+  visited_rooms = set()
+  player.current_room = world.starting_room
+  visited_rooms.add(player.current_room)
 
 
 
 
-##### discover_string = instead of using directions, use ids
-## try to find the nearest id which has an open pathway
+  ## BEGIN OUR TRAVELS
+
+  ## Checking to see if I've reached each room
+  while len(visited_rooms) < 499:
+    exits = player.current_room.get_exits()
+    print('')
+    print('')
+    print(len(visited_rooms))
+    print('we ended up back at the top of if', exits)
+    near_rooms = []
+    untouched_exits = []
+
+    ## Develop near_rooms
+    ## Take current id, and each exit direction. Get new room id
+    ## Check to see if any exits are already navigated
+    for exit in exits:
+      #NXT ROOM   ##GRABBING ROOM OBJ IN MAP     # DIRECTION
+      exit_check = map_dict[player.current_room.id][exit]
+      print('EXIT CHECK', exit_check)
+      ## Add any room directions not visited
+      if exit_check not in visited_rooms:
+        untouched_exits.append(exit)
+    print('ALTERED EXITS', untouched_exits)
+    ## Check to see if any exits are a pivot node
+    #### Checks to see if current room is a pivot node
+    print('exit in for loop', exit)
+    if player.current_room.id in dead_ends:
+      print('Passed dead_end check')
+      possible_pivots = []
+      for exit in untouched_exits:
+        for route in dead_ends[player.current_room.id]:
+          if route == exit:
+            possible_pivots.append(route)
+      print('here be possible pivots', possible_pivots)
+      get_move = possible_pivots[0]
+      print(get_move)
+      ## This takes first first possible_pivot on this room, and moves towards that dead_end
+      player.travel(get_move)
+
+    # If no pivot node at this id, go down any untouched path
+    if len(untouched_exits) > 0:
+      ## Randomizing direction
+      direction_roll = untouched_exits[random.randint(0, len(untouched_exits)-1)]
+      player.travel(direction_roll)
+
+    elif len(untouched_exits) == 0:
+      direction_roll = exits[random.randint(0, len(exits)-1)]
+      player.travel(direction_roll)
+    else:
+      print('fuck you extra')
+
+    ## We've hit this point if 
+
+    ## Check to see if current_room.id exists
+    ## Randomly go down another path
+
+
+  ## 
+  ##
+  # player.current_room.print_room_description(player)
+  pass
 
 
 
-# TRAVERSAL TEST - DO NOT MODIFY
-visited_rooms = set()
-player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
 
-for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
+
+## Use this to run repeated traversals in one code line.
+def repeat_traversal(number):
+  pass
+
+# # TRAVERSAL TEST - DO NOT MODIFY
+# visited_rooms = set()
+# player.current_room = world.starting_room
+# visited_rooms.add(player.current_room)
+
+# for move in traversal_path:
+#     player.travel(move)
+#     visited_rooms.add(player.current_room)
+
+discover_map()
+find_deadends(map_dict)
+warrior_traversal()
+
+#######
+# UNCOMMENT TO WALK AROUND
+#######
+# player.current_room.print_room_description(player)
+
+# while True:
+#   cmds = input("-> ").lower().split(" ")
+#   if cmds[0] in ["n", "s", "e", "w"]:
+#     player.travel(cmds[0], True)
+#   elif cmds[0] == "q":
+#     break
+#   else:
+#     print("I did not understand that command.")
+
+
+
+### Create Dictionary that is used as a "success" measure of the application (len of dictionary)
+## Success metric
+# visited = {
+#   000: {n, s, e, w}
+#   }
+## Create counter for # of steps
+## Open Map file
+## Parse map file for all dead-ends & add these to an array
+# For each room in the array, find their turning point (Backtrack until 3 direction are found). 434 (dead-end) 281: n = 350
+## Save direction + pivot node in dictionary
+## pivot_dict = {
+#     000: s, n
+#   }
+## Need to come up with case if multiple dead end routes.
+#
+## Perform DFT, except always prioritize deadend nodes, if not, proceed to next stack node
+## Success?
+## Randomize order of direction when 3/4 way option?
+## when 2 ways, always go new direction
+## When 3 ways, check for pivot node, if not roll for initial direction (not previous)
+
+## ?? I'm not revisiting?
+# Stack??
+# ALWAYS priotize pivots, do anything on this route after that until route is dead
+# Go back to my last turn and find my path forward
+
+# ??? 
+
+## Do some sort of searching? to figure out a random direction
+## Everytime a Dead-end pivot node is reached, proceed to end of path, then turn back.
+
+
+
 
 
 # if len(visited_rooms) == len(room_graph):
@@ -219,110 +414,3 @@ for move in traversal_path:
   # Traversal??
   # 000: 
   # 001: 
-
-
-
-
-# LET'S DO THE REAL THING
-def find_deadends(full_map):
-  ## This method uses the map_dict to determine traversal path
-  ## It finds all dead ends
-  ## It back tracks for each dead end to it's pivot location (next room with 3 exits)
-  prev_direction = None
-  dead_ends = {}
-
-  opposites = {
-    'n': 's',
-    'e': 'w',
-    's': 'n',
-    'w': 'e'
-  }
-
-  # direction = None
-  direction_room = None
-  prev_direction = None
-  prev_room = None
-
-  # This grabs every dead end room id
-  ## This should be recursion if I had 1 sliver of a brain
-  for room in map_dict:
-    num_exits = map_dict[room]
-    if len(num_exits) == 1:
-      prev_room = room
-      for key in map_dict[room]:
-        prev_direction = key
-      direction_room = map_dict[room][prev_direction]
-      while len(map_dict[direction_room]) is 2:
-        for path in map_dict[direction_room]:
-          ## Makes sure you aren't moving backwards. Opposite choice, should move you back down the path you came from
-          if path not in map_dict[direction_room]:
-            print('fuck you')
-          elif map_dict[direction_room][path] != prev_room:
-            prev_room = direction_room
-            prev_direction = path
-            direction_room = map_dict[prev_room][prev_direction]
-
-      if direction_room in dead_ends:
-        dead_ends[direction_room].append(opposites[prev_direction])
-      else:
-        dead_ends[direction_room] = [opposites[prev_direction]]
-  print('dead-ends', dead_ends)
-
-def pivot_path(direction):
-  pass
-
-# while len(visited_rooms) < 500:
-#   ## Get all deadends
-
-#   pass
-
-
-
-
-#######
-# UNCOMMENT TO WALK AROUND
-#######
-player.current_room.print_room_description(player)
-discover_map()
-find_deadends(map_dict)
-while True:
-  cmds = input("-> ").lower().split(" ")
-  if cmds[0] in ["n", "s", "e", "w"]:
-    player.travel(cmds[0], True)
-  elif cmds[0] == "q":
-    break
-  else:
-    print("I did not understand that command.")
-
-
-
-### Create Dictionary that is used as a "success" measure of the application (len of dictionary)
-## Success metric
-# visited = {
-#   000: {n, s, e, w}
-#   }
-## Create counter for # of steps
-## Open Map file
-## Parse map file for all dead-ends & add these to an array
-# For each room in the array, find their turning point (Backtrack until 3 direction are found). 434 (dead-end) 281: n = 350
-## Save direction + pivot node in dictionary
-## pivot_dict = {
-#     000: s, n
-#   }
-## Need to come up with case if multiple dead end routes.
-#
-## Perform DFT, except always prioritize deadend nodes, if not, proceed to next stack node
-## Success?
-## Randomize order of direction when 3/4 way option?
-## when 2 ways, always go new direction
-## When 3 ways, check for pivot node, if not roll for initial direction (not previous)
-
-## ?? I'm not revisiting?
-# Stack??
-# ALWAYS priotize pivots, do anything on this route after that until route is dead
-# Go back to my last turn and find my path forward
-
-# ??? 
-
-## Do some sort of searching? to figure out a random direction
-## Everytime a Dead-end pivot node is reached, proceed to end of path, then turn back.
